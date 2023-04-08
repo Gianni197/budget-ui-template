8e5d39faa57a6a4e18ef6c4a8f3caefc9766c214
Gianni197 <gianni.iellamo@lernen.siw.swiss>
2023-04-08 15:43:30 +0200

    save08.04.2023

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -1,42 +1,149 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { addMonths, set } from 'date-fns';
-import { ModalController } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
-import { Expense } from '../../shared/domain';
-import { save } from 'ionicons/icons';
+import { Category, CategoryCriteria, Expense, ExpenseCriteria, SortOption } from '../../shared/domain';
+import { formatPeriod } from '../../shared/period';
+import { BehaviorSubject, debounce, from, groupBy, interval, mergeMap, Subject, takeUntil, toArray } from 'rxjs';
+import { ToastService } from '../../shared/service/toast.service';
+import { ExpenseService } from '../expense.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CategoryService } from '../../category/category.service';
 
-  @Component({
+interface ExpenseGroup {
+  date: string;
+  expenses: Expense[];
+}
+@Component({
   selector: 'app-expense-overview',
-    templateUrl: './expense-list.component.html',
+  templateUrl: './expense-list.component.html',
 })
-export class ExpenseListComponent {
+export class ExpenseListComponent implements OnInit, OnDestroy {
   date = set(new Date(), { date: 1 });
+  expenseGroups: ExpenseGroup[] | null = null;
+  readonly initialSort = 'date,desc';
+  lastPageReached = false;
+  loading = false;
+  searchCriteria: ExpenseCriteria = { page: 0, size: 25, sort: this.initialSort };
 
-constructor(private readonly modalCtrl: ModalController) {}
+  categories: Category[] = [];
 
-  addMonths = (number: number): void => {
-  this.date = addMonths(this.date, number);
-  };
-    categories: any;
-    expenseForm: any;
+  readonly searchForm: FormGroup;
+  readonly sortOptions: SortOption[] = [
+    { label: 'Created at (newest first)', value: 'createdAt,desc' },
+    { label: 'Created at (oldest first)', value: 'createdAt,asc' },
+    { label: 'Name (A-Z)', value: 'name,asc' },
+    { label: 'Name (Z-A)', value: 'name,desc' },
+    { label: 'Date (newest first)', value: 'date,desc'},
+    { label: 'Date (oldest first)', value: 'date,asc'},
+  ];
+  private readonly unsubscribe = new Subject<void>();
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly toastService: ToastService,
+    private readonly categoryService: CategoryService,
+    private readonly expenseService: ExpenseService,
+    private readonly formBuilder: FormBuilder,
+  ) {
+    this.searchForm = this.formBuilder.group({name: [], categoryIds: [], sort: [this.initialSort]});
+    this.searchForm.valueChanges
+      .pipe(
+        takeUntil(this.unsubscribe),
+        debounce((value) => (value.name?.length ? interval(400) : interval(0)))
+      )
+      .subscribe((value) => {
+        this.searchCriteria = { ...this.searchCriteria, ...value, page: 0 };
+        this.loadExpenses();
+      })
+  }
+
+
 
   async openModal(expense?: Expense): Promise<void> {
-    const modal = await this.modalCtrl.create({
-      component: ExpenseModalComponent,
-      componentProps: { expense: expense ? { ...expense } : {} },
+    const modal = await this.modalCtrl.create({ component: ExpenseModalComponent,
+      componentProps: { expense: expense ? { ...expense } : {}},
     });
     modal.present();
     const { role } = await modal.onWillDismiss();
+    if (role === 'refresh') this.loadExpenses();
     console.log('role', role);
   }
 
-  cancel() {
 
+  private loadAllCategories(): void {
+    const pageToLoad = new BehaviorSubject(0);
+    pageToLoad
+      .pipe(mergeMap((page) => this.categoryService.getCategories({ page, size: 25, sort: 'name,asc'})))
+      .subscribe({
+        next: (categories) => {
+          if (pageToLoad.value === 0) this.categories = [];
+          this.categories.push(...categories.content);
+          if (!categories.last) pageToLoad.next(pageToLoad.value + 1);
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not load categories', error),
+      });
   }
+  private loadExpenses(next: () => void = () => {}): void {
+    this.searchCriteria.yearMonth = formatPeriod(this.date);
+    if (!this.searchCriteria.categoryIds?.length) delete this.searchCriteria.categoryIds;
+    if (!this.searchCriteria.name) delete this.searchCriteria.name;
+    this.loading = true;
+    this.expenseService
+      .getExpenses(this.searchCriteria)
+      .pipe(
+        mergeMap((expensePage) => {
+          this.lastPageReached = expensePage.last;
+          next();
+          this.loading = false;
+          if (this.searchCriteria.page === 0 || this.expenseGroups) this.expenseGroups = [];
+          return from(expensePage.content).pipe(
+            groupBy((expense) => expense.date),
+            mergeMap((group) => group.pipe(toArray()))
+          );
+        })
+      )
+      .subscribe({
+        next: (expenses: Expense[]) => {
+          const expenseGroup: ExpenseGroup = {
+            date: expenses[0].date,
+            expenses: this.sortExpenses(expenses),
+          };
+          const expenseGroupWithSameDate = this.expenseGroups!.find((other) => other.date === expenseGroup.date);
+          if (!expenseGroupWithSameDate) this.expenseGroups!.push(expenseGroup);
+          else
+            expenseGroupWithSameDate.expenses = this.sortExpenses([
+              ...expenseGroupWithSameDate.expenses,
+              ...expenseGroup.expenses,
+            ]);
+        },
+        error: (error) => {
+          this.toastService.displayErrorToast('Could not load expenses', error);
+          this.loading = false;
+        },
+      });
+  }
+  addMonths = (number: number): void => {
+    this.date = addMonths(this.date, number);
+    this.reloadExpenses();
+  };
 
-  save() {
+  private sortExpenses = (expenses: Expense[]): Expense[] => expenses.sort((a, b) => a.name.localeCompare(b.name));
 
+  ngOnInit(): void {
+    this.loadExpenses();
+    this.loadAllCategories();
+  }
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+  loadNextExpensePage($event: any) {
+    this.searchCriteria.page++;
+    this.loadExpenses(() => ($event as InfiniteScrollCustomEvent).target.complete());
+  }
+  reloadExpenses($event?: any): void {
+    this.searchCriteria.page = 0;
+    this.loadExpenses(() => ($event ? ($event as RefresherCustomEvent).target.complete() : {}));
   }
 }
-
 
--- src/app/expense/expense-modal/expense-modal.component.ts
+++ src/app/expense/expense-modal/expense-modal.component.ts
@@ -1,87 +1,112 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { BehaviorSubject, filter, from, mergeMap } from 'rxjs';
+import { CategoryModalComponent } from '../../category/category-modal/category-modal.component';
 import { ActionSheetService } from '../../shared/service/action-sheet.service';
-import { filter, from, mergeMap, pipe, Subject } from 'rxjs';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Category, CategoryCriteria, Expense, SortOption } from '../../shared/domain';
-import { ToastService } from '../../shared/service/toast.service';
-import { save } from 'ionicons/icons';
+import { Category, Expense, ExpenseUpsertDto, SortOption } from '../../shared/domain';
 import { CategoryService } from '../../category/category.service';
+import { ToastService } from '../../shared/service/toast.service';
+import { FormBuilder, FormGroup, isFormGroup, Validators } from '@angular/forms';
+import { ExpenseService } from '../expense.service';
+import { format, formatISO, parseISO } from 'date-fns';
+import { ca } from 'date-fns/locale';
+
 
 @Component({
   selector: 'app-expense-modal',
   templateUrl: './expense-modal.component.html',
 })
 export class ExpenseModalComponent implements OnInit {
-  categories: Category[] | null = null;
-  readonly expenseForm: FormGroup;
-  submitting = false;
-  readonly initialSort = 'name,asc';
-  searchCriteria: CategoryCriteria = { page: 0, size: 25, sort: this.initialSort };
-  readonly searchForm: FormGroup;
-  readonly sortOptions: SortOption[] = [
-    { label: 'Created at (newest first)', value: 'createdAt,desc' },
-    { label: 'Created at (oldest first)', value: 'createdAt,asc' },
-    { label: 'Name (A-Z)', value: 'name,asc' },
-    { label: 'Name (Z-A)', value: 'name,desc' },
-  ];
-  private readonly unsubscribe = new Subject<void>();
+  ngOnInit(): void {
+    const { id, amount, category, date, name } = this.expense;
+    if (category) this.categories.push(category);
+    if (id) this.expenseForm.patchValue({ id, amount, categoryId: category?.id, date, name });
+    this.loadAllCategories();
+  }
 
-  // Passed into the component by the ModalController, available in the ngOnInit
+
+  categories: Category[] = [];
   expense: Expense = {} as Expense;
 
   constructor(
     private readonly actionSheetService: ActionSheetService,
-    private readonly categoryService: CategoryService,
-    private readonly formBuilder: FormBuilder,
     private readonly modalCtrl: ModalController,
-    private readonly toastService: ToastService
+    private readonly categoryService: CategoryService,
+    private readonly toastService: ToastService,
+    private readonly formBuilder: FormBuilder,
+    private readonly expenseService: ExpenseService,
   ) {
-    this.searchForm = this.formBuilder.group({ name: [], sort: [this.initialSort] });
     this.expenseForm = this.formBuilder.group({
-      id: [], // hidden
+      id: [], //hidden
+      categoryId: [],
       name: ['', [Validators.required, Validators.maxLength(40)]],
+      amount: [],
+      date: [formatISO(new Date())],
     });
   }
+  readonly expenseForm: FormGroup;
+  submitting = false;
 
-  ngOnInit(): void {
-    this.loadAllCategories();
+  private loadAllCategories(): void {
+    const pageToLoad = new BehaviorSubject(0);
+    pageToLoad
+      .pipe(mergeMap((page) => this.categoryService.getCategories({ page, size: 25, sort: 'name,asc'})))
+      .subscribe({
+        next: (categories) => {
+          if (pageToLoad.value === 0) this.categories = [];
+          this.categories.push(...categories.content);
+          if (!categories.last) pageToLoad.next(pageToLoad.value + 1);
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not load categories', error),
+      });
   }
-
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
   }
 
   save(): void {
     this.submitting = true;
-    this.categoryService.upsertCategory(this.expenseForm.value).subscribe({
-      next: () => {
-        this.toastService.displaySuccessToast('Category saved');
-        this.modalCtrl.dismiss(null, 'refresh');
-        this.submitting = false;
-      },
-      error: (error) => {
-        this.toastService.displayErrorToast('Could not save category', error);
-        this.submitting = false;
-      },
-    });
-  }
-
-
-
-  // Passed into the component by the ModalController, available in the ngOnInit
-
-  delete(): void {
-    from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this category?'))
-      .pipe(filter((action) => action === 'delete'))
+    this.expenseService
+      .upsertExpense({ ...this.expenseForm.value, date: format(parseISO(this.expenseForm.value.date), 'yyyy-MM-dd')})
       .subscribe({
         next: () => {
-          this.modalCtrl.dismiss(null, 'delete');
+          this.toastService.displaySuccessToast('Expense saved');
+          this.modalCtrl.dismiss(null, 'refresh');
+          this.submitting = false;
+        },
+        error: (error) => {
+          this.toastService.displayErrorToast('Could not save category', error);
+          this.submitting = false;
         },
       });
   }
 
-  private loadAllCategories() {
-
+  delete(): void {
+    from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this expense?'))
+      .pipe(
+        filter((action) => action === 'delete'),
+        mergeMap(() => {
+          this.submitting = true;
+          return this.expenseService.deleteExpense(this.expense.id!);
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.toastService.displaySuccessToast('Expense deleted');
+          this.modalCtrl.dismiss(null, 'refresh');
+          this.submitting = false;
+        },
+        error: (error) => {
+          this.toastService.displayErrorToast('Could not delete expense', error),
+            this.submitting = false;
+        },
+      })
+  }
+  async showCategoryModal(): Promise<void> {
+    const categoryModal = await this.modalCtrl.create({ component: CategoryModalComponent });
+    categoryModal.present();
+    const { role } = await categoryModal.onWillDismiss();
+    if (role === 'refresh') this.loadAllCategories();
+    console.log('role', role);
   }
 }
